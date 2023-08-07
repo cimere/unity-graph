dc2ba516d967d184187b004b4cbdaf8af00c3b60
Ciro Meregalli <ciro.meregalli@gmail.com>
2023-08-07 22:38:51 +0200

    add time

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -4,23 +4,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Graph : MonoBehaviour {
-    [SerializeField] private Transform pointPrefab;
-    [SerializeField, Range(10, 100)] private int resolution = 10;
+    [SerializeField]
+    private Transform pointPrefab;
+    
+    [SerializeField, Range(10, 100)]
+    private int resolution = 10;
+
+    private Transform[] points;
     private void Awake() {
         float step = 2f / resolution;
         var position = Vector3.zero;
         var scale = Vector3.one * step;
-        
-        for (int i = 0; i < resolution; i++) {
-            Transform point = Instantiate(pointPrefab);
-            
+
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i] = Instantiate(pointPrefab);
             position.x = ((i + 0.5f) * step - 1f);
-            position.y = position.x * position.x * position.x;
-            
             point.localPosition = position;
             point.localScale = scale;
-            
             point.SetParent(transform, false);
+        }
+    }
+
+    private void Update() {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
         }
     }
 }
