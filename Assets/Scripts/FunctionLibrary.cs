45f2c346f0b8a5666df1ec6e0e80f52ba2e8ca09
Ciro Meregalli <ciro.meregalli@gmail.com>
2023-08-12 22:44:25 +0200

    add sphere function

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -10,9 +10,10 @@ public static class FunctionLibrary {
     public enum FunctionName {
         Wave,
         MultiWave,
-        Ripple
+        Ripple,
+        Sphere
     };
-    private static Function[] functions = { Wave, MultiWave, Ripple };
+    private static Function[] functions = { Wave, MultiWave, Ripple, Sphere };
 
     public static Function GetFunction(FunctionName name) {
         return functions[(int)name];
@@ -44,6 +45,16 @@ public static class FunctionLibrary {
         p.y = Sin(PI * (4f * d - t));
         p.y /= 1f + 10f * d;
         p.z = v;
+        return p;
+    }
+
+    public static Vector3 Sphere(float u, float v, float t) {
+        float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+        float s = r * Cos(0.5f * PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r * Sin(PI * 0.5f * v);
+        p.z = s * Cos(PI * u);
         return p;
     }
 }
